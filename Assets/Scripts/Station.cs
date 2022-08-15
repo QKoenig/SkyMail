9082e89b065f54054b29a5065487a6a31af79811
QKoenig <49294921+QKoenig@users.noreply.github.com>
2022-08-15 13:18:19 -0400

    trainstation time

--- Assets/Scripts/Station.cs
+++ Assets/Scripts/Station.cs
@@ -6,6 +6,8 @@ using TMPro;
 public class Station : MonoBehaviour
 {
     public TextMeshProUGUI text1;
+    public TextMeshProUGUI text2;
+    public TextMeshProUGUI text3;
     public Stop stop;
     // Start is called before the first frame update
     void Start()
@@ -19,7 +21,7 @@ public class Station : MonoBehaviour
         int minutes = ((int)(stop.timeUntilNextStop / 60));
         int seconds = ((int)(stop.timeUntilNextStop % 60));
         int hundreths = (int)((stop.timeUntilNextStop - Mathf.Floor(stop.timeUntilNextStop)) * 100f);
-        text1.text = stop.timeUntilNextStop > 0 && stop.timeUntilNextStop + stop.stopTime < stop.totalTimeForPath ? string.Format("<mspace=mspace=2.2>{0,2}</mspace>:<mspace=mspace=2.2>{1,2}</mspace>", (minutes > 0 ? minutes : seconds).ToString("D2"), (minutes > 0 ? seconds : hundreths).ToString("D2")) :
+        text1.text = text2.text = text3.text = stop.timeUntilNextStop > 0 && stop.timeUntilNextStop + stop.stopTime < stop.totalTimeForPath ? string.Format("<mspace=mspace=2.2>{0,2}</mspace>:<mspace=mspace=2.2>{1,2}</mspace>", (minutes > 0 ? minutes : seconds).ToString("D2"), (minutes > 0 ? seconds : hundreths).ToString("D2")) :
             (int)(Time.time * 2f) % 2 == 0 ? "ARR" : "";
     }
 }
